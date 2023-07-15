4a54975b7239588f63b8a6675657b1b8bd6f7da5
Nurudeen <nurudeen.jola@gmail.com>
2023-07-15 23:22:49 +0100

    Copy - Cut - Paste

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -1,7 +1,7 @@
-
-#!/usr/bin/python3
 word = "Holberton"
-# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
+
+# WORD TO BE PRINTED CUTTING PART OF IT AND PASTING IT
+
 print ("First 3 letters: {}\n".format (word[:3]))
 print ("Last 2 letters: {}\n".format (word[-2:]))
 print ("Middle word: {}\n".format (word[1:-1]))
