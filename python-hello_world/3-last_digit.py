0887c72d81d8db110aae50b02d0e523a54cec7f9
Nurudeen <nurudeen.jola@gmail.com>
2023-07-16 17:05:00 +0100

    Positive anything is better than negative nothing

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -2,7 +2,7 @@
 import random
 number = random.randint(-10000, 10000)
 # YOUR CODE HERE
-last_digit = abs(number) % 10;
+last_digit = abs(number) % 10
 if number < 0:
     last_digit = last_digit*(-1)
     if last_digit < 6 and last_digit != 0:
