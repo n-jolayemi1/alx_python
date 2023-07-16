ccc79330c9bdfcb43810bccef082ffff261594bb
Nurudeen <nurudeen.jola@gmail.com>
2023-07-16 17:09:15 +0100

    Positive anything is better than negative nothing

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -6,11 +6,11 @@ last_digit = abs(number) % 10
 if number < 0:
     last_digit = last_digit*(-1)
     if last_digit < 6 and last_digit != 0:
-        print("Last digit of {:d} is {:d} and is less than 6 and not 0".format(number, last_digit))
+        print("Last digit of {} is {} and is less than 6 and not 0".format(number, last_digit))
 elif number >= 0:
     if last_digit > 5:
-        print("Last digit of {:d} is {:d} and is greater than 5".format(number, last_digit))
+        print("Last digit of {} is {} and is greater than 5".format(number, last_digit))
     if last_digit == 0:
-        print("Last digit of {:d} is {:d} and is 0".format(number, last_digit))
+        print("Last digit of {} is {} and is 0".format(number, last_digit))
     if last_digit < 6 and last_digit != 0:
-        print("Last digit of {:d} is {:d} and is less than 6 and not 0".format(number, last_digit))
+        print("Last digit of {} is {} and is less than 6 and not 0".format(number, last_digit))
