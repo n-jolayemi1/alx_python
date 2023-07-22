94e37886956670a0b59448148d9e9743fbae3978
Nurudeen <nurudeen.jola@gmail.com>
2023-07-22 10:38:59 +0100

    Hello world

--- python-hello_world/0-print.py
+++ python-hello_world/0-print.py
@@ -1,17 +1,2 @@
 #!/usr/bin/python3
 print('"Programming is like building a multilingual puzzle')
-""" import random
-number = random.randint(-10000, 10000)
-# YOUR CODE HERE
-last_digit = abs(number) % 10
-if number < 0:
-    last_digit = last_digit*(-1)
-    if last_digit < 6 and last_digit != 0:
-        print("Last digit of {} is {} and is less than 6 and not 0".format(number, last_digit))
-elif number >= 0:
-    if last_digit > 5:
-        print("Last digit of {} is {} and is greater than 5".format(number, last_digit))
-    if last_digit == 0:
-        print("Last digit of {} is {} and is 0".format(number, last_digit))
-    if last_digit < 6 and last_digit != 0:
-        print("Last digit of" number "is {} and is less than 6 and not 0".format(, last_digit)) """
