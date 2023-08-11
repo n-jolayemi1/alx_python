59c52f85b6dabf7e3d9f43cf159c19441ead2560
Nurudeen <nurudeen.jola@gmail.com>
2023-08-11 21:35:17 +0100

    python inheritance

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -2,28 +2,11 @@
  """
 
 class BaseGeometry:
-    """ 
-    creating a public instance method that will raise an exception message
-      """
-    def area(self):
-        raise Exception("area() is not implemented")
-    
-    def integer_validator(self, name="", value=0):
 
-        if not isinstance(value, int):
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
-
-""" 
-creating a class rectangle that inherits from BaseGeometry
-
-return: width and height 
-"""
-class Rectangle(BaseGeometry):
     """ 
     declaring a attribute that will remove the init subclass
       """
+
     def __dir__(cls):
         # Get all the attributes, including __init_subclass__
         attributes = super().__dir__()
@@ -35,9 +18,33 @@ class Rectangle(BaseGeometry):
         return new_attributes
     
     """ 
-    defininig an initial instance inside rectangle
+    creating a public instance method that will raise an exception message
+      """
+    def area(self):
+        raise Exception("area() is not implemented")
+    
+    def integer_validator(self, name="", value=0):
+
+        if not isinstance(value, int) or value <= 0:
+            raise TypeError("{} must be a positive integer".format(name))
+        
+
+class Rectangle(BaseGeometry):
+
     """
-    def __init__(self, width=None, height=None):
+    Represents a rectangle with width and height.
+    """
+
+    def __init__(self, width, height):
+        """ 
+        Inherit a Rectangle from the Basegeometry class
+
+        args: width (int): The width of the rectangle.
+                height (int): The height of the rectangle.
+
+            Raises:
+                ValueError: If width or height are not positive integers.
+        """
         self.integer_validator("width", width)
         self.integer_validator("height", height)
         self.__width = width
